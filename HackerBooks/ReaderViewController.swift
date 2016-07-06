9eb37ef0633d4d89d8fcb346658365c8e49faaf3
icapa <ivan.olea@gmail.com>
2016-07-06 20:16:26 +0200

    Probado la descarga de recursor por requerimiento, GCD para pdf

--- HackerBooks/ReaderViewController.swift
+++ HackerBooks/ReaderViewController.swift
@@ -15,6 +15,7 @@ class ReaderViewController: UIViewController {
     
     @IBOutlet weak var pdfViewer: UIWebView!
     
+    @IBOutlet weak var activityView: UIActivityIndicatorView!
     init(model: Book){
         self.model = model
         super.init(nibName: nil, bundle: nil)
@@ -26,15 +27,12 @@ class ReaderViewController: UIViewController {
     
     //MARK: Sync View & Model
     func syncModelWithView(){
-        guard let pdf = model.pdfFile else{
-            pdfViewer.loadHTMLString("<H1> Error loading pdf </H1>", baseURL: NSURL())
-            return
-        }
-        pdfViewer.loadData(pdf,
-                          MIMEType: "application/pdf",
-                          textEncodingName: "UTF-8",
-                          baseURL: NSURL())
         
+        activityView.startAnimating()
+        activityView.hidden = false
+
+        // Lanza en background, para que deje actualizar el UIView
+        downloadPDF()
     }
     
     override func didReceiveMemoryWarning() {
@@ -45,12 +43,21 @@ class ReaderViewController: UIViewController {
     //MARK: - View life cycle
     override func viewDidLoad(){
         super.viewDidLoad()
+        
     }
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        syncModelWithView()
+    }
+    
     override func viewWillAppear(animated: Bool) {
         super.viewWillDisappear(animated)
+        // Delegado de aviso
+        pdfViewer.delegate = self
+        
         let nc = NSNotificationCenter.defaultCenter()
         nc.addObserver(self, selector: #selector(bookDidChange), name: BookDidChangeNotification, object: nil)
-        syncModelWithView()
+        //syncModelWithView()
         
     }
     
@@ -65,10 +72,42 @@ class ReaderViewController: UIViewController {
     */
     //MARK: - Notification
     func bookDidChange(notification: NSNotification){
+        
         let info = notification.userInfo
         let book = info![BookKey] as? Book
         self.model = book!
+        
+        
+        
         syncModelWithView()
     }
 
 }
+//MARK: - Extension UIWebViewDelegate
+extension ReaderViewController: UIWebViewDelegate{
+    func webViewDidFinishLoad(webView: UIWebView) {
+        activityView.stopAnimating()
+        activityView.hidden = true
+    }
+}
+//MARK: - Extension GCD
+extension ReaderViewController{
+    func downloadPDF(){
+        let  download = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT,0)
+        let elBloque : dispatch_block_t = {
+            guard let pdf = self.model.pdfFile else{
+                self.pdfViewer.loadHTMLString("<H1> Error loading pdf </H1>", baseURL: NSURL())
+                return
+            }
+            self.pdfViewer.loadData(pdf,
+                               MIMEType: "application/pdf",
+                               textEncodingName: "UTF-8",
+                               baseURL: NSURL())
+
+        }
+        dispatch_async(download, elBloque)
+        
+    }
+}
+//MARK: - GCD
+
